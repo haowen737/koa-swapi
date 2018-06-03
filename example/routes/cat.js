49bdff22fbab73826a11bf5f59b67c1dd8eac628
Haowen <haowen737@gmail.com>
2018-06-03 21:52:22 +0800

    feat: use hapi route style

--- example/routes/cat.js
+++ example/routes/cat.js
@@ -4,21 +4,23 @@ const Joi = require('joi')
 module.exports = [{
   method: 'get',
   path: '/cat/:id',
-  id: 'getCat',
-  summary: '获得一只帅气猫',
-  description: '想获得一只帅气猫的时候可以调用这个接口',
-  validate: {
-    params: {
-      id: Joi.string().required().min(2).max(4).description('猫的id')
-    },
-    query: {
-      name: Joi.string().required().min(3).max(100).description('猫的名字'),
-      sex: Joi.any().required().valid(['0', '1']).description('猫的性别, 0:男, 1:女')
-    },
-    // type: 'form',
-    output: {
-      200: {
-        body: Joi.string()
+  config: {
+    id: 'getCat',
+    summary: '获得一只帅气猫',
+    description: '想获得一只帅气猫的时候可以调用这个接口',
+    validate: {
+      params: {
+        id: Joi.string().required().min(2).max(4).description('猫的id')
+      },
+      query: {
+        name: Joi.string().required().min(3).max(100).description('猫的名字'),
+        sex: Joi.any().required().valid(['0', '1']).description('猫的性别, 0:男, 1:女')
+      },
+      // type: 'form',
+      output: {
+        200: {
+          body: Joi.string()
+        }
       }
     }
   }
--- example/routes/dog.js
+++ example/routes/dog.js
@@ -4,35 +4,39 @@ const Joi = require('joi')
 module.exports = [{
   method: 'get',
   path: '/dog',
-  summary: '获得一只呆狗',
-  description: '要获得一只呆狗的时候可以调这个接口',
-  validate: {
-    query: {
-      name: Joi.string().required().min(3).max(100).description('狗的名字'),
-    },
-    type: 'form',
-    output: {
-      200: {
-        body: Joi.string()
-      }
-    }
-  },
-}, {
-  method: 'put',
-  path: '/dog',
-  summary: '创建一只呆狗',
-  description: '要创建一只呆狗的时候可以调这个接口',
-  validate: {
-    payload: Joi.object({
-      a: Joi.number(),
-      b: Joi.number().required()
-    }).label('呆狗对象').description('呆狗对象的详情'),
-    output: {
-      200: {
-        body: {
-          name: Joi.string()
+  config: {
+    summary: '获得一只呆狗',
+    description: '要获得一只呆狗的时候可以调这个接口',
+    validate: {
+      query: {
+        name: Joi.string().required().min(3).max(100).description('狗的名字'),
+      },
+      type: 'form',
+      output: {
+        200: {
+          body: Joi.string()
         }
       }
     }
-  },
+  }
+}, {
+  method: 'put',
+  path: '/dog',
+  config: {
+    summary: '创建一只呆狗',
+    description: '要创建一只呆狗的时候可以调这个接口',
+    validate: {
+      payload: Joi.object({
+        a: Joi.number(),
+        b: Joi.number().required()
+      }).label('呆狗对象').description('呆狗对象的详情'),
+      output: {
+        200: {
+          body: {
+            name: Joi.string()
+          }
+        }
+      }
+    }
+  }
 }]
